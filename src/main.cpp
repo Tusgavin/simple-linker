e71f89192b1863b18b5509354a4a5fcf322bd5fa
Lucas Augusto <nutkaugusto@gmail.com>
2021-08-10 18:29:18 -0300

    input

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,42 @@
 #include <string>
 #include <regex>
 
+#include "ligador.h"
+
+
+
 #define error(msg) { std::cout << "eRRor: " << msg << std::endl; exit(EXIT_FAILURE); }
 
-#include "ligador.h"
+
+
+void split(const std::string &s, char delim, std::vector<std::string> &elems) {
+    std::stringstream ss(s);
+    std::string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+}
+
+std::vector<std::string> split(const std::string &s, char delim) {
+    std::vector<std::string> elems;
+    split(s, delim, elems);
+    return elems;
+}
+
+
 
 int main(int argc, char *argv[]) {
 
+    std::vector<File *> assembly_files;
+
     for (int i=1; i<argc; i++) {
+
+        std::vector<std::pair<std::string, int>> labels_in_file;
+        std::string assembly_code;
+        int assembly_code_size;
+        int assembly_code_start;
+        int file_index = i-1;
+
         const std::string fileName = std::string(argv[i]);
         std::string line;
         std::fstream myFile;
@@ -20,47 +49,66 @@ int main(int argc, char *argv[]) {
         myFile.open(fileName);
 
         if (myFile.is_open()) {
+
+            int line_index = 0;
+
             while (getline (myFile,line) ) {
                 std::stringstream ss(line);
                 std::string item = "";
 
-                std::cout << line << std::endl;
+                if (line == "MV-EXE") continue;
 
-                for (size_t i=0; i<=line.size(); i++) {
+                const char label_detector = ':';
 
-                    if (i == line.size() && item.size()!=0) {
-                        // this->fileInstructions.push_back(item);
-                        std::cout << item << std::endl;
-                        item = "";
-                    }
+                if (line[0] == label_detector) {
+                    line.erase(remove(line.begin(), line.end(), label_detector), line.end());
+                    
+                    std::vector<std::string> elems = split(line, ' ');
+                    // std::cout << "primeiro split" << std::endl;
+                    // for(auto k=0; k<elems.size(); k++){
+                    //     std::cout << elems[k] << std::endl;
+                    // }
+                    std::pair<std::string, int> label;
+                    label.first = elems[0];
+                    label.second = std::stoi(elems[1]);
+                    labels_in_file.push_back(label);
+                } else if (line_index == 0 && (isalpha(line[0]) || std::isdigit(line[0]))) {
+                    line_index = 1;
 
-                    if (line[i] == ';') {
-                        
-                        if (item.size() != 0) {
-                            // this->fileInstructions.push_back(item);
-                            std::cout << item << std::endl;
-                            item = "";
-                        }
-                        break;
-                    }
-
-                    if ((!isalpha(line[i]) && !std::isdigit(line[i])) || line[i]==' ') {
-                        if (item.size() != 0) {
-                            // this->fileInstructions.push_back(item);
-                            std::cout << item << std::endl;
-                            item = "";
-                        }
-                    } else {
-                        item += line[i];
-                    }
-
+                    std::vector<std::string> elems = split(line, ' ');
+                    // std::cout << "segundo split" << std::endl;
+                    // for(auto k=0; k<elems.size(); k++){
+                    //     std::cout << elems[k] << std::endl;
+                    // }
+                    
+                    assembly_code_size = std::stoi(elems[0]);
+                    assembly_code_start = std::stoi(elems[3]);
+                } else if (line_index == 1  && (isalpha(line[0]) || std::isdigit(line[0]))) {
+                    line_index = -1;
+                    assembly_code = line;
                 }
+                
+
             }
 
             myFile.close();
         } else error("could not open file ! =x"); 
 
+        // for(auto k=0; k<labels_in_file.size(); k++){
+        //     std::cout << labels_in_file[k].first << " e " << labels_in_file[k].second << std::endl;
+        // }
+        // std::cout << assembly_code << std::endl;
+        // std::cout << assembly_code_size << std::endl;
+        // std::cout << assembly_code_start << std::endl;
+        // std::cout << file_index << std::endl;
+        
+
+        File* file = new File(labels_in_file, assembly_code, assembly_code_size, assembly_code_start, file_index);
+        assembly_files.push_back(file);
+
     }
+
+    Linker* linker = new Linker(assembly_files);
 
     return 0;
 }
